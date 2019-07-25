505367267f6d8b83770652c00a35c0c868f1ba60
Iuliia Dutchak <iuliia.dutchak@ev-box.com>
2019-07-25 11:32:06 +0200

    add route samples

--- src/components/ListsNav.vue
+++ src/components/ListsNav.vue
@@ -1,8 +1,10 @@
 // ListsNav.vue
-
 <template v-if="dataLoaded">
   <main>
     <h2>{{ description }}</h2>
+    <router-link to="/foo">Go to Foo</router-link>
+    <router-link to="/bar">Go to Bar</router-link>
+    <router-view></router-view>
     <ul>
       <li
         v-for="(tableLink, tableName) of tablesList"
@@ -25,9 +27,38 @@
   </main>
 </template>
 
+<style scoped>
+  ul {
+    list-style: none;
+    display: flex;
+    padding: 20px 0;
+  }
+
+  ul li a {
+    padding: 5px 10px;
+    border-right: 1px solid darkgray;
+    cursor: pointer;
+  }
+
+  ul li a:hover,
+  ul li.active a {
+    background-color: aliceblue;
+  }
+
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.5s;
+  }
+
+  .fade-enter,
+  .fade-leave-to {
+    opacity: 0;
+  }
+</style>
+
 <script>
-import _ from 'lodash';
-import ListTable from './ListTable.vue';
+import _ from "lodash";
+import ListTable from "./ListTable.vue";
 
 export default {
   data: () => {
@@ -39,30 +70,31 @@ export default {
       dataLoaded: false
     };
   },
-  created: function () {
-      let vm = this;
-      fetch("https://swapi.co/api/")
-        .then(response => response.json())
-        .then(data => {
-            vm.tablesList = data;
-            vm.updateActiveList();
-            vm.dataLoaded = true;
-            })
-        .catch(error => console.error('error', error));
+  created: function() {
+    let vm = this;
+    fetch("https://swapi.co/api/")
+      .then(response => response.json())
+      .then(data => {
+        vm.tablesList = data;
+        vm.updateActiveList();
+        vm.dataLoaded = true;
+      })
+      .catch(error => console.error("error", error));
   },
   methods: {
     updateActiveList(listName) {
       this.activeList = listName || Object.keys(this.tablesList)[0];
       this.getactiveListData(listName);
     },
-    getactiveListData(){
-        return fetch(`https://swapi.co/api/${this.activeList}/`).then(response => response.json())
-                                                              .then(data => this.activeListData = data.results)
-                                                              .catch(error => console.error('error', error));
+    getactiveListData() {
+      return fetch(`https://swapi.co/api/${this.activeList}/`)
+        .then(response => response.json())
+        .then(data => (this.activeListData = data.results))
+        .catch(error => console.error("error", error));
     }
   },
   filters: {
-    capitalize: (v) => {
+    capitalize: v => {
       return _.capitalize(v);
     }
   },
@@ -71,32 +103,3 @@ export default {
   }
 };
 </script>
-
-<style scoped>
-ul {
-  list-style: none;
-  display: flex;
-  padding: 20px 0;
-}
-
-ul li a {
-  padding: 5px 10px;
-  border-right: 1px solid darkgray;
-  cursor: pointer;
-}
-
-ul li a:hover,
-ul li.active a {
-  background-color: aliceblue;
-}
-
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s;
-}
-
-.fade-enter,
-.fade-leave-to {
-  opacity: 0;
-}
-</style>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import Vue from "vue";
-import App from "./App";
+import VueRouter from "vue-router";
+import App from "./App.vue";
 
 Vue.config.productionTip = false;
+Vue.use(VueRouter);
+
+const Foo = { template: "<div>foo</div>" };
+const Bar = { template: "<div>bar</div>" };
+
+const routes = [
+  { path: "/foo", component: Foo },
+  { path: "/bar", component: Bar }
+];
+
+const router = new VueRouter({
+  routes // short for `routes: routes`
+});
 
 /* eslint-disable no-new */
 new Vue({
   el: "#app",
   template: "<App/>",
-  components: { App }
+  components: { App },
+  router
 });
