df5485f042fc9f4ab819ffd6a31ebee468416ac2
Iuliia Dutchak <iuliia.dutchak@ev-box.com>
2019-07-25 11:34:50 +0200

    move routes to separate file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import routes from "./routes";
 import App from "./App.vue";
 
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
-
-const Foo = { template: "<div>foo</div>" };
-const Bar = { template: "<div>bar</div>" };
-
-const routes = [
-  { path: "/foo", component: Foo },
-  { path: "/bar", component: Bar }
-];
 
 const router = new VueRouter({
   routes // short for `routes: routes`
